201d89e5974764488e529b735a2d3cd166b3cfde
Le Nguyen Hai Dang <danglnh.ce190707@gmail.com>
2024-03-21 22:23:58 +0700

    push elephant

---
+++ Elephant/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int x;
+
+int main () {
+    cin >> x;
+
+    if (x % 5 == 0)
+        cout << x / 5;
+    else
+        cout << x / 5 + 1;
+
+    return 0;
+}
