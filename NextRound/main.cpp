a8dc275da8328200eda343c919570294c5cc36e2
Le Nguyen Hai Dang <danglnh.ce190707@gmail.com>
2024-03-21 20:05:30 +0700

    push next round

---
+++ NextRound/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, k, temp, value, ans = 0;
+vector<int> arr;
+
+int main () {
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        arr.push_back(temp);
+    }
+
+    value = arr[k - 1];
+
+    for (int v : arr) {
+        if (v > 0 && v >= value)
+            ans++;
+    }
+
+    cout << ans;
+    return 0;
+}
